8466e8fdab016462598a4e00799c6f92475c2234
Robin Meis <hc.devel@yahoo.de>
2015-01-05 16:59:06 +0100

    Update

---
+++ SiriAPI8/action.py
@@ -0,0 +1,38 @@
+class action: #Stores actions for SiriAPI8
+    def __init__ (self):
+        self.actions = []
+        self.actions.append({'find': "", 'call': self.error})
+        return
+
+    def add (self, find, call): #Add action
+        self.actions.append({'find': [find], 'call': call})
+        return (len(self.actions) - 1)
+
+    def modify (self, id, find, call): #Modify action by id
+        try:
+            if (find == -1): #Keep old values if not overwritten
+                find = self.actions[id]['find']
+
+            if (call == -1):
+                call = self.actions[id]['call']
+
+            self.actions[id] = {'find': [find], 'call': call}
+            return (True)
+        except:
+            return (False)
+
+    def remove (self, id): #Remove action by id
+        if (id > 0): #Make it impossible to delete the not found rule
+            try:
+                del self.actions[id]
+                return (True)
+            except:
+                return (False)
+        else:
+            return (False)
+
+    def list (self): #List all action
+        return (self.actions)
+
+    def error (self, q, wildcards_found): #Error message if not overwritten
+        print ("SiriAPI8 error: No action defined for " + q)
---
+++ SiriAPI8/search.py
@@ -0,0 +1,56 @@
+class search:
+    def __init__ (self, SiriAPI):
+        self.SiriAPI = SiriAPI
+
+    def search (self, q): #search for matching
+        q_search = q.lower().replace(self.SiriAPI.keyword + " ", "")
+        print (q_search)
+        for keywords in self.SiriAPI.action.actions[:]: #Complicated search algorithm ;)
+            for keyword in keywords['find'][:]:
+                for find in keyword[:]:
+                    if (isinstance(find, list) == False):
+                        if (find == q_search):
+                            return (keywords['call'](q, None))
+                    else:
+                        found = True
+                        have_to_follow = True
+                        cursor = 0
+                        wildcard_counter = -1
+                        wildcard_start = 0
+                        wildcard_end = -1
+                        wildcards_found = {}
+                        for search in find[:]:
+                            if (search == '*'):
+                                wildcard_counter += 1
+                                have_to_follow = False
+                                wildcard_start = cursor
+                            else:
+                                position = q_search.find(search, cursor)
+
+                                if (position == cursor):
+                                    cursor += len(search) + 1
+                                    wildcard_end = position
+                                elif (position > cursor and have_to_follow == False):
+
+                                    cursor = position + len(search) + 1
+                                    have_to_follow = True
+                                    wildcard_end = position - 1
+                                else:
+                                    found = False
+                                    break
+
+                                if (wildcard_end > -1):
+                                    wildcards_found[wildcard_counter] = q_search[wildcard_start:wildcard_end]
+                                    wildcard_start = 0
+                                    wildcard_end = 0
+
+                                if (find[-1] == "*" and find[-2] == search):
+                                    wildcards_found[wildcard_counter + 1] = q_search[cursor:]
+                                    wildcard_counter += 1
+
+
+                        if (found == True):
+                            return (keywords['call'](q, wildcards_found))
+                            return
+
+        return (self.SiriAPI.action.actions[0]['call'](q, None))
