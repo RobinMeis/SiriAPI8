7dba501813138a5fe80dfd0c6bf40cfda89738a8
Robin Meis <hc.devel@yahoo.de>
2015-01-05 16:58:49 +0100

    Update

--- example.py
+++ example.py
@@ -1,6 +1,14 @@
 from SiriAPI8.SiriAPI import *
 
-siriAPI = SiriAPI("my_email@me.com", "my_password")
+def hello (q, wildcards): #Here could react you script
+    print ("Q: " + q)
+    print (wildcards)
+
+siriAPI = SiriAPI("my_icloud_mail@me.com", "my_password")
+
+siriAPI.action.add([['hello', '*']], hello)
 siriAPI.connect()
+
+print (siriAPI.action.list())
 input("Press any key...\n")
 siriAPI.disconnect()
