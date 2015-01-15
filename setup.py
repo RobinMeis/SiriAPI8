a00a8b5a8bab36b0752159c33e41565c6e610675
Robin Meis <hc.devel@yahoo.de>
2015-01-15 18:44:51 +0100

    Setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='SiriAPI8',
+      version='1.0.3',
+      description='SiriAPI8 - use Siri to interact with Python scripts',
+      author='Robin Meis',
+      author_email='hc.devel@yahoo.de',
+      url='https://github.com/HcDevel/SiriAPI8',
+      packages=['SiriAPI8'],
+      platforms = ('Any'))
