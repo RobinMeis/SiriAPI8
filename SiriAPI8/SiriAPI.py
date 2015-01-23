605eb844780938d9b1f6cb35bd071d3bb6962aac
Robin Meis <hc.devel@yahoo.de>
2015-01-23 21:40:26 +0100

    Hopefully keep connection alive

--- SiriAPI8/SiriAPI.py
+++ SiriAPI8/SiriAPI.py
@@ -11,7 +11,7 @@ class SiriAPI:
         self.username = username
         self.password = password
         self.keyword = "iphone"
-        self.version = "0.0.0" #TODO: Change version number on new release
+        self.version = "1.0.4" #TODO: Change version number on new release
         self.connection = None
         self.fetch = None
         self.stop = True
@@ -49,7 +49,7 @@ class SiriAPI:
         for num in data[0].split():
             self.connection.store(num, '+FLAGS', '\\Deleted')
         self.connection.expunge()
-        self.fetch = threading.Thread(target=self.fetch)
+        self.fetch = threading.Thread(target=self.__fetch)
         self.fetch.start()
         return (True)
 
@@ -70,25 +70,26 @@ class SiriAPI:
     def __fetch(self):
         time.sleep(1)
         while (self.stop == False):
-                recent = self.connection.recent() #Check for new notes
-                if (recent[1][0] != None):
-                    time.sleep(1) #Sleeps prevent crashes (crazy and I don't know why)
-                    typ, data = self.connection.search(None, 'ALL', 'SUBJECT "' + self.keyword + '"') #Fetch new notes
-                    for num in data[0].split():
-                        raw_email = self.connection.fetch(num, '(RFC822)')[1][0][1]
-                        email_message = email.message_from_bytes(raw_email)
-                        if email_message.is_multipart(): #Parse content
-                            for payload in email_message.get_payload():
-                                text = payload.get_payload()
-                        else:
-                            text = email_message.get_payload()
+            recent = self.connection.recent() #Check for new notes
+            if (recent[1][0] != None):
+                time.sleep(1) #Sleeps prevent crashes (crazy and I don't know why)
+                typ, data = self.connection.search(None, 'ALL', 'SUBJECT "' + self.keyword + '"') #Fetch new notes
+                for num in data[0].split():
+                    raw_email = self.connection.fetch(num, '(RFC822)')[1][0][1]
+                    email_message = email.message_from_bytes(raw_email)
+                    if email_message.is_multipart(): #Parse content
+                        for payload in email_message.get_payload():
+                            text = payload.get_payload()
+                    else:
+                        text = email_message.get_payload()
 
-                        text = text.replace("\n","").replace("\r","")
-                        self.__search.search(text)
-                        time.sleep(1)
-                        self.connection.store(num, '+FLAGS', '\\Deleted')
-                        self.connection.expunge()
-                time.sleep(1)
+                    self.connection.store(num, '+FLAGS', '\\Deleted')
+                    self.connection.expunge()
+                    text = text.replace("\n","").replace("\r","")
+                    self.__search.search(text)
+                    time.sleep(1)
+            self.connection.noop()
+            time.sleep(1)
         return()
 
     def __connection_check(self):
